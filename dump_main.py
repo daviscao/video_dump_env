238915dc7a3d8af0ddd72c393d61f64a979f3298
daviscao <312788062@qq.com>
2016-04-29 10:37:06 +0800

    redirect some info to log
    print config info to dump.log file

--- dump_main.py
+++ dump_main.py
@@ -1,40 +1,66 @@
-'''Main dump file:dump_main.py.
+'''Main dump file for video team.
 
-author : daviscao@zhaoxin.com
-date   : 2016.4.15
-config and driver dump_env_base.py'''
-
+version: 2.0
+author : daviscao
+date   : 20160428
+config and driver dump_env_base.py
+'''
 # -*- coding: utf-8 -*-
 import re
 import dump_env_base
+import os
+import sys
 
-
-if __name__ == '__main__':
-    dump_env_cfg = 'dump_env_cfg.txt'
+def parse_cfgfile(dump_env_cfg):
     cfg_dict = dict()
     with open(dump_env_cfg,'r') as foo:
         cfg_lines = foo.readlines()
     if len(cfg_lines) == 0:
         print('Please prepare your dump_env_cfg.txt file ...')
-        input('print any key to continue ...')
+        sys.exit(0)
     else:
         for each in cfg_lines:
-            if re.search('=',each):
-                each = each.strip()
-                each = re.sub(' ','',each)
+            if re.match('\s*//.*',each):
+                continue                            #jump the comment line
+            elif re.search('=',each):
+                each = re.sub('\s*//.*','',each)    #remove the comment at the end of the line
+                each = each.strip()                 #remove the line-break
+                each = re.sub('\s','',each)         #remove any space in the line
                 key,value = re.split('=',each)
                 if key == 'cmdidx_list':
-                    cfg_dict[key] = map(int,value.split(','))
+                    cfg_dict[key] = list()
+                    for idxstr in re.split(',',value):
+                        match = re.match('(\w+)-(\w+)',idxstr)
+                        if match:
+                            span_l = range(int(match.group(1)),int(match.group(2))+1)
+                        else:
+                            span_l = range(int(idxstr),int(idxstr)+1)
+                        cfg_dict[key] += span_l
                 else:
                     cfg_dict[key] = value
+    cfg_dict['userinifile']         = cfg_dict['dump_cfg_path']+os.sep+cfg_dict['userinifile']
+    cfg_dict['userbatfile']         = cfg_dict['dump_cfg_path']+os.sep+cfg_dict['userbatfile']
+    cfg_dict['basic_binary_path']   = cfg_dict['dump_path']+os.sep+cfg_dict['binary_num']
+    cfg_dict['abt_binary_path']     = cfg_dict['abt_path']+os.sep+cfg_dict['binary_num']
 
-    dump_env_inst = dump_env_base.dump_env(project            = cfg_dict['project'],
-                                           binary             = cfg_dict['binary'],
-                                           batch              = cfg_dict['batch'],
-                                           inifile            = cfg_dict['inifile'],
-                                           cmdidx_list        = cfg_dict['cmdidx_list'],
-                                           result_path        = cfg_dict['result_path'],
-                                           datmode            = cfg_dict['datmode'],
-                                           usermodedrivername = cfg_dict['usermodedrivername'])
-    dump_env_inst.kickoff_dumpenv()
-    
+    for each in cfg_dict:
+        print(each,cfg_dict[each])
+        try:
+            if re.search(r'\\',cfg_dict[each]):
+                if not os.path.exists(cfg_dict[each]):
+                    print("    *E: the path '%s' does not exists ...\n"%cfg_dict[each])
+        except:
+            pass
+    return cfg_dict
+
+if __name__ == '__main__':
+    stdout_old = sys.stdout
+    with open('dump.log','w') as log:
+        sys.stdout = log
+        print('\n****************** parse the config *********************\n')
+        dump_env_cfg = 'dump_env_cfg.txt'
+        dump_cfg_dict = parse_cfgfile(dump_env_cfg)
+        print('\n******************* start dump vector *******************\n')
+    sys.stdout = stdout_old
+    dump_inst = dump_env_base.dump_env(dump_cfg_dict)
+    dump_inst.kickoff_dumpenv()
